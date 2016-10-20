272090c74437f3d5237b052c4db934148b28bab0
renan028 <renan028@gmail.com>
2016-10-19 22:14:39 -0200

    add sensorvalue1910, change sec to hour, change save name (date first)

--- plot_sensor.m
+++ plot_sensor.m
@@ -3,8 +3,8 @@ function [] = plot_sensor(sensor, sensor_name, directory_to_save)
 sensor_hour = hour2num(sensor{3});
 fig = figure;
 plot(sensor_hour, sensor{1})
-title_str = strcat(sensor_name,strcat('_',sensor{2}(1,:)));
+title_str = strcat(strcat(sensor{2}(1,:),'-'),sensor_name);
 title(title_str)
 xlabel('time')
 ylabel(strcat(sensor_name,' values'))
-print(fig, strcat(directory_to_save,title_str),'-dpng')
+print(fig, strcat(directory_to_save,title_str,'-dpng')
